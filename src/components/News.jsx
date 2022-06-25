e0e1cc1839e604b42dbb446eb5400e2977e807cc
kevinsmasters <gmail@kevinmasters.com>
2022-06-25 09:07:11 -0400

    progress news api

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,10 +2,41 @@ import React from 'react'
 import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
 import moment from 'moment';
 
+import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 
-const News = () => {
+const { Text, Title } = Typography;
+const { Option } = Select;
+const demoImage = 'https://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg';
+const News = ( { simplified }) => {
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 6 : 12});
+  
+  if(!cryptoNews?.value) return 'Loading...';
+
   return (
-    <div>News</div>
+    <Row gutter={[ 24, 24 ]}>
+      {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>{news.name}</Title>
+                <img src={news?.image?.thumbnail?.contentUrl || demoImage} alt={news.name} /> 
+              </div>
+              <p>
+                {news.description > 100 ? `${news.description.substring(0, 100)}...` : news.description}
+              </p>
+              <div className="provider-container">
+                <div>
+                  <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage } alt="your mom"/>
+                  <Text className="provider-name">{news.provider[0]?.name}</Text>
+                </div>
+                <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
   )
 }
 
