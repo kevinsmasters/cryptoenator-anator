b7a080992f87c1ce23e796843f457a52d043282a
kevinsmasters <gmail@kevinmasters.com>
2022-06-24 22:09:56 -0400

    start adding in news api

--- src/components/Craptocurrencies.jsx
+++ src/components/Craptocurrencies.jsx
@@ -23,9 +23,12 @@ const Craptocurrencies = ({simplified}) => {
 
   return (
     <>
-      <div className="search-crypto">
-        <Input placeholder="search cryptocurrency" onChange={(e)=> setSearchTerm(e.target.value)} />
-      </div>
+      {!simplified && (
+        <div className="search-crypto">
+          <Input placeholder="search cryptocurrency" onChange={(e)=> setSearchTerm(e.target.value)} />
+        </div>
+      )}
+      
       <Row gutter={[32, 32]} className="crypt-card-container">
         {cryptos?.map((currency)=>(
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
