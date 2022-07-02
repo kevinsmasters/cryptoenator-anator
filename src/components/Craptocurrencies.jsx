1289d3994596f36c76b9474d00e4b653487c006a
kevinsmasters <gmail@kevinmasters.com>
2022-07-02 19:13:28 -0400

    fixes uuid vs id bug

--- src/components/Craptocurrencies.jsx
+++ src/components/Craptocurrencies.jsx
@@ -28,11 +28,11 @@ const Craptocurrencies = ({simplified}) => {
           <Input placeholder="search cryptocurrency" onChange={(e)=> setSearchTerm(e.target.value)} />
         </div>
       )}
-      
+      {console.log(cryptos)}
       <Row gutter={[32, 32]} className="crypt-card-container">
         {cryptos?.map((currency)=>(
-          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
-            <Link to={`/crypto/${currency.id}`}>
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.uuid}>
+            <Link to={`/crypto/${currency.uuid}`}>
               <Card 
                 title={`${currency.rank}. ${currency.name}`}
                 extra={<img className="crypto-image" src={currency.iconUrl} />}  
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -14,11 +14,15 @@ export const cryptoApi = createApi({
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
         getCryptos: builder.query({
-            query: (count) => createRequest(`/coins?limit=${count}`)
-        })
+            query: (count) => createRequest(`/coins?limit=${count}`),
+        }),
+        getCryptoDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`),
+        }),
     })
 })
 
 export const {
     useGetCryptosQuery,
+    useGetCryptoDetailsQuery
 } = cryptoApi;
